4d78fcb75370ddb073f10b1762af5c01259bc5fd
udayraj10 <cudayabi44@gmail.com>
2026-01-31 16:17:04 +0530

    Secure Firebase credentials using environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDgdCFSBwG1KC2Dr_NSUr_84tiJpCUeFLA",
-    authDomain: "student-complaint-portal-f40c0.firebaseapp.com",
-    projectId: "student-complaint-portal-f40c0",
-    storageBucket: "student-complaint-portal-f40c0.firebasestorage.app",
-    messagingSenderId: "406320277895",
-    appId: "1:406320277895:web:63400abbe5c0d1750a8b22",
-    measurementId: "G-5Q0410SGXC"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
